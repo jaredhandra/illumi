229a614ae577db0f7501fdd83e852214d8d0a1c6
Jared Handra <jaredhandra@mac.lan>
2025-03-05 15:36:47 -0500

    setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import FeatureFlagSetForm from "@/components/FeatureFlagSetForm.vue";
+</script>
+
+<template>
+  <div class="layout-container">
+    <FeatureFlagSetForm />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FeatureFlagSetForm.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    thing
+  </div>
+</template>
+
+<style scoped>
+
+</style>
